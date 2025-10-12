722d5ab3bc0ff66c9c83c9587b9e49c0546e0936
jiyoung Lee <ghqkdaos4767@gmail.com>
2025-10-13 00:48:01 +0900

    고딕게임버전_v1

--- pofol.js
+++ pofol.js
@@ -1,9 +1,9 @@
 // aos선언
 //   AOS.init();
- AOS.init({
-      duration: 1200,
-      once: true
-    });
+AOS.init({
+    duration: 1200,
+    once: true
+});
 // preloader
 setTimeout(() => {
     const loading = document.querySelector('.loading');
@@ -20,7 +20,9 @@ setTimeout(() => {
 //cursor
 let mouseX = 0;
 let mouseY = 0;
-
+document.querySelector(".inner").addEventListener('scroll',()=>{
+    console.log($(this).scrolltop)
+});
 // 마우스 위치 저장
 document.addEventListener('mousemove', (e) => {
     mouseX = e.clientX;
@@ -52,29 +54,41 @@ document.addEventListener('click', () => {
     shootBullet(mouseX, mouseY);
 });
 
-// 아래서 위로 서서히 드러나는 fade 효과
-// document.addEventListener("DOMContentLoaded", () => {
-//     const observer = new IntersectionObserver((entries) => {
-//         entries.forEach(entry => {
-//             if (entry.isIntersecting) {
-//                 entry.target.classList.add('visible');
-//                 // 한번 보이면 관찰 해제 (한 번만 애니메이션 줄 경우)
-//                 observer.unobserve(entry.target);
-//             }
-//             else {
-//                 entry.target.classList.remove('visible');
-//             }
-//         });
-//     }, {
-//         threshold: 0.6 // 요소가 60% 보이면 동작
-//     });
-
-//     // 모든 .showUp 요소에 대해 observer 연결
-//     document.querySelectorAll('.showUp').forEach(el => observer.observe(el));
-// });
-
-// slick
 $(document).ready(function () {
+// header
+// header 스크롤시 디자인 변경
+console.log(scrollY);
+$(window).on('scroll', function () {
+    if ($(this).scrollTop()) {
+        $('#header').addClass('active');
+        // document.querySelector('#about').scrollIntoView();
+    } 
+    else {
+        $('#header').removeClass('active');
+    }
+});
+// header scroll반응
+let lastScroll = 0;
+const header = document.getElementById('header');
+
+window.addEventListener('scroll', () => {
+    const currentScroll = window.scrollY;
+    console.log(currentScroll);
+    if (currentScroll > lastScroll) {
+        // 아래로 스크롤
+        $('#header').removeClass('show');
+        // document.querySelector('#about').scrollIntoView();
+        // $('#header').addClass('hide');
+    } else if(currentScroll < lastScroll){
+        // 위로 스크롤
+        $('#header').removeClass('hide');
+        $('#header').addClass('show');
+        // document.querySelector('#main').scrollIntoView();
+    }
+    lastScroll = currentScroll;
+});
+// slick
+
 
     $('.autoplay').slick({
         centerMode: true,          // 중앙 정렬 모드 활성화
@@ -98,38 +112,8 @@ $(document).ready(function () {
         arrows: false
     });
 
-    // header 스크롤시 디자인 변경
-    $(window).on('scroll', function () {
-        if ($(window).scrollTop()) {
-            $('#header').addClass('active');
-        } else {
-            $('#header').removeClass('active');
-        }
-    });
 });
-// header scroll반응
 
-let lastScroll = 0;
-const header = document.getElementById('header');
-
-window.addEventListener('scroll', () => {
-    const currentScroll = window.scrollY;
-    if (currentScroll > lastScroll) {
-        // 아래로 스크롤
-        $('#header').removeClass('show');
-        $('#header').addClass('hide');
-        // header.classList.remove('show');
-        // header.classList.add('hide');
-
-    } else {
-        // 위로 스크롤
-        $('#header').addClass('show');
-        $('#header').removeClass('hide');
-        // header.classList.add('show');
-        // header.classList.remove('hide');
-    }
-    lastScroll = currentScroll;
-});
 
 // 모달 modal
 
@@ -169,7 +153,7 @@ $(document).on('click', '.character-box .egg', function () {
         let img = $('<img>').attr('src', imgPath).attr('alt', `character${characterNum}-${i}`);
         modalBox.append(img);
     }
-     $('.modal').css('display', 'flex'); // flex로 변경
+    $('.modal').css('display', 'flex'); // flex로 변경
     // 모달 보이기
     $('.modal').fadeIn();
 });
@@ -204,7 +188,7 @@ function fillProgress(duration = 20000) {
         const left = maxLeft * percent;
         $runner.css('left', left + 'px');
     }, interval);
-    
+
 }
 
 // main-visual이 화면에 등장하면 실행
@@ -216,7 +200,7 @@ const about_observer = new IntersectionObserver((entries) => {
         if (entry.isIntersecting && !played) {
             fillProgress(5000);
             played = true;
-            
+
         }
     });
 }, {
@@ -225,3 +209,40 @@ const about_observer = new IntersectionObserver((entries) => {
 });
 
 about_observer.observe(about_section);
+
+// shortcut img change
+const naverShortcutButton = document.querySelector(".naver-shortcut-button");
+const isaacShortcutButton = document.querySelector(".isaac-shortcut-button");
+
+naverShortcutButton.addEventListener('click',()=>{
+    $('.naver-shortcut').addClass('view');
+    $('.issac-shortcut').removeClass('view');
+});
+isaacShortcutButton.addEventListener('click',()=>{
+    $('.issac-shortcut').addClass('view');
+    $('.naver-shortcut').removeClass('view');
+});
+
+// 키워드 박스  하이라이트
+const k1box =$(".k1box");
+const k2box =$(".k2box");
+const k3box = $(".k3box");
+
+k1box.on('mouseover',()=>{
+    $('.k1').addClass('highlight');
+});
+k1box.on('mouseout',()=>{
+    $('.k1').removeClass('highlight');
+});
+k2box.on('mouseover',()=>{
+    $('.k2').addClass('highlight');
+});
+k2box.on('mouseout',()=>{
+    $('.k2').removeClass('highlight');
+});
+k3box.on('mouseover',()=>{
+    $('.k3').addClass('highlight');
+});
+k3box.on('mouseout',()=>{
+    $('.k3').removeClass('highlight');
+});
