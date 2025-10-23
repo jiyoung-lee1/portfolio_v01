4a5bca91a7275a706447ab5e95320bd039db8382
jiyoung Lee <ghqkdaos4767@gmail.com>
2025-10-23 22:34:02 +0900

    monami favicon 추가, video 자동재생 js 추가

--- js/monami.js
+++ js/monami.js
@@ -18,3 +18,19 @@ popCloseBtn.addEventListener('click',()=>{
     document.querySelector(".pop-up-btn").style.display='none';
 });
 
+
+const video = document.querySelector('#player');
+const videoSection = document.querySelector('.video');
+
+window.addEventListener('scroll', () => {
+  const rect = videoSection.getBoundingClientRect();
+  const windowHeight = window.innerHeight;
+
+  // 화면에 절반 이상 보이면 재생
+  if (rect.top < windowHeight / 2 && rect.bottom > windowHeight / 2) {
+    video.play();
+  } else {
+    video.pause();
+  }
+});
+
